e321b7c8fbc15e3ab5fdfc391c450c49d0caf9d6
Paul Clark <pdclark@brainstormmedia.com>
2013-09-02 02:43:11 -0800

    Add Google Fonts api fallback in case of API failure.

--- classes/styles-google-fonts.php
+++ classes/styles-google-fonts.php
@@ -20,8 +20,14 @@ class Styles_Google_Fonts extends Styles_Fonts {
 	 */
 	protected $families;
 
+	/**
+	 * @var string path to JSON backup of Google API response. In case API fails or is unavailable.
+	 */
+	protected $api_fallback_file;
+
 	public function __construct() {
 		$this->cache_interval = apply_filters( 'styles_google_fonts_cache_interval', 60*60*24*15 ); // 15 days
+		$this->api_fallback_file = dirname( dirname( __FILE__ ) ) . '/js/google-fonts-api-fallback.json';
 	}
 
 	/**
@@ -30,24 +36,26 @@ class Styles_Google_Fonts extends Styles_Fonts {
 	public function get_fonts() {
 		// Return from cache if available
 		$this->fonts = get_transient( 'styles_google_fonts' );
+
 		if ( false !==  $this->fonts ) { return $this->fonts; }
 
 		// Bail if no API key is set
 		$api_key = apply_filters( 'styles_google_font_api', '' );
-		if ( empty( $api_key ) ) { return false; }
+		if ( empty( $api_key ) ) { return $this->get_api_fallback(); }
 
 		// Construct request
 		$url = add_query_arg( 'sort', apply_filters( 'styles_google_font_sort', 'popularity' ), self::font_api_url );
 		$url = add_query_arg( 'key', $api_key, $url );
 		$response = wp_remote_get( $url );
 
-		if ( is_a( $response, 'WP_Error') ) { return false; }
+		// Response is an error. Use the fallback file
+		if ( is_a( $response, 'WP_Error') ) { return $this->get_api_fallback(); }
 
 		$fonts = json_decode( $response['body'] );
 
+		// Response is empty. Use the fallback file.
 		if ( !is_array( $fonts->items ) ) {
-			// @todo check to see if this messes up the caching above
-			$this->fonts = null;
+			return $this->get_api_fallback();
 		}
 
 		set_transient( 'styles_google_fonts', $this->fonts, $this->cache_interval );
@@ -66,6 +74,24 @@ class Styles_Google_Fonts extends Styles_Fonts {
 		}
 
 		return $this->families;
+	}
+
+	/**
+	 * If the we don't have a Google API key, or the request fails,
+	 * use the contents of this file instead.
+	 * 
+	 * @todo Rework this and set_api_fallback to use transients and write to disk using WP_Filesystem so we don't have two caching mechanisms going on at once.
+	 */
+	public function get_api_fallback() {
+		return json_decode( file_get_contents( $this->api_fallback_file ) );
+	}
+
+	/**
+	 * Save Google Fonts API response to file for cases where we
+	 * don't have an API key or the API request fails
+	 */
+	public function set_api_fallback() {
+		file_put_contents( $this->api_fallback_file, json_encode( $this->fonts ) );
 	}
 	
 }

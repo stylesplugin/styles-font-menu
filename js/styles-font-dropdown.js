8b0f6bc4705f571a9c0138cfbbfb4fb0c5426824
Paul Clark <pdclark@brainstormmedia.com>
2013-09-03 02:05:36 -0800

    Speed up building of Google Fonts option list; only build it once. Resolves #1.

--- js/styles-font-dropdown.js
+++ js/styles-font-dropdown.js
@@ -1,39 +1,53 @@
 jQuery( document ).ready( function( $ ){
 
+	// Add Google Fonts and Chosen to select elements
 	$('select.styles-font-dropdown').stylesFontDropdown();
 
 });
 
 (function( $, styles_google_options ) {
+
+	/**
+	 * Build Google Fonts option list only once
+	 */
+	var google_options = "<optgroup class='google-fonts' label='Google Fonts'>";
+	for (var i=0; i < styles_google_options.fonts.length; i++){
+		google_options += "<option value='" + JSON.stringify( styles_google_options.fonts[i] ) + "'>" + styles_google_options.fonts[i].font_family + "</option>";
+	}
+	google_options += "</optgroup>";
+
+	/**
+	 * Define jQuery plugin to act on and attach to select elements
+	 */
 	$.stylesFontDropdown = function(element, options) {
 
-		var plugin = this;
-		var defaults = {};
-		plugin.settings = {};
+		var plugin = this,
+				$element = $(element);
 
-		var $element = $(element),
-			   element = element;
+		/**
+		 * Default settings. Override by passing object to stylesFontDropdown()
+		 */
+		var defaults = {
+					"chosen_settings": {
+						"allow_single_deselect": true,
+						"inherit_select_classes": true
+					}
+				};
+
+		plugin.settings = {};
 
 		plugin.init = function() {
 				plugin.settings = $.extend({}, defaults, options);
 
 				plugin.populate_google_fonts();
 
-				$element.chosen({
-					"allow_single_deselect": true,
-					"inherit_select_classes": true
-				});
+				$element.chosen( plugin.settings.chosen_settings );
 		};
 
 		plugin.populate_google_fonts = function() {
-			var google_options = "<optgroup class='google-fonts' label='Google Fonts'>";
-
-			$.each( styles_google_options.fonts, function( i, options ){
-				google_options += "<option value='" + JSON.stringify(options) + "'>" + options.font_family + "</option>";
-			});
-			google_options += "</optgroup>";
-
 			$element.append( google_options ).each( function(){
+				// If a selected option is set in <option data-selected="XXX">, select it.
+				// @todo Not sure why this is here. Carried over from old Styles text selector. Check back when connecting to database.
 				var selected = $(this).data('selected');
 				$(this).find( 'option[value="' + selected + '"]' ).attr('selected', 'selected');
 			} );
@@ -44,6 +58,10 @@ jQuery( document ).ready( function( $ ){
 
 	};
 
+	/**
+	 * Attach this plugin instance to the target elements
+	 * Access later with $('select.styles-font-dropdown').data('stylesFontDropdown');
+	 */
 	$.fn.stylesFontDropdown = function(options) {
 		return this.each(function() {
 			if (undefined === $(this).data('stylesFontDropdown')) {
@@ -53,4 +71,4 @@ jQuery( document ).ready( function( $ ){
 		});
 	};
 
-})(jQuery, styles_google_options );
+})( jQuery, styles_google_options );

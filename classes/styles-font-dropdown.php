ee7800a60a40af73a3b6539ed79794cfba566da6
Paul Clark <pdclark@brainstormmedia.com>
2013-09-02 20:55:19 -0800

    Move admin methods into Styles_Font_Dropdown_Admin class

--- classes/styles-font-dropdown.php
+++ classes/styles-font-dropdown.php
@@ -2,6 +2,7 @@
 
 if ( !class_exists( 'Styles_Font_Dropdown' ) ) :
 
+require_once dirname(__FILE__) . '/styles-font-dropdown-admin.php';
 require_once dirname(__FILE__) . '/styles-fonts.php';
 require_once dirname(__FILE__) . '/styles-standard-fonts.php';
 require_once dirname(__FILE__) . '/styles-google-fonts.php';
@@ -11,21 +12,29 @@ add_action( 'init', create_function( '', 'new Styles_Font_Dropdown();'), 11 );
 class Styles_Font_Dropdown {
 
 	/**
-	 * @var Styles_Standard_Fonts Web standard font families and CSS font stacks
+	 * @var string The plugin version.
+	 */
+	var $version = '0.1';
+
+	/**
+	 * @var Styles_Font_Dropdown_Admin Methods for WordPress admin user interface.
+	 */
+	var $admin;
+
+	/**
+	 * @var Styles_Standard_Fonts Web standard font families and CSS font stacks.
 	 */
 	var $standard_fonts;
 
 	/**
-	 * @var Styles_Google_Fonts Connects to Google Font API
+	 * @var Styles_Google_Fonts Connects to Google Font API.
 	 */
 	var $google_fonts;
 
-	var $version = '0.1';
-
 	/**
-	 * Set with site_url() because we might not be running as a plugin
+	 * Set with site_url() because we might not be running as a plugin.
 	 * 
-	 * @var string URL for the styles-font-dropdown directory
+	 * @var string URL for the styles-font-dropdown directory.
 	 */
 	var $plugin_directory;
 
@@ -37,21 +46,18 @@ class Styles_Font_Dropdown {
 	var $plugin_basename;
 
 	/**
-	 * print_scripts() runs as late as possible to avoid processing Google Fonts
-	 * This prevents running multiple times
+	 * print_scripts() runs as late as possible to avoid processing Google Fonts.
+	 * This prevents running multiple times.
+	 * 
 	 * @var bool Whether we have already registered scripts or not.
 	 */
 	var $scripts_printed = false;
-
-	/**
-	 * @var string Slug for readme at /wp-admin/plugins.php?page=$readme_page_slug
-	 */
-	var $readme_page_slug = 'styles-font-dropdown';
 
 	public function __construct() {
 		$this->plugin_directory = site_url( str_replace( ABSPATH, '', dirname( dirname( __FILE__ ) ) ) );
 		$this->plugin_basename = plugin_basename( dirname( dirname( __FILE__ ) ) . '/plugin.php' );
 
+		$this->admin = new Styles_Font_Dropdown_Admin( $this );
 		$this->google_fonts = new Styles_Google_Fonts();
 		$this->standard_fonts = new Styles_Standard_Fonts();
 
@@ -59,24 +65,8 @@ class Styles_Font_Dropdown {
 		 * Output dropdown menu anywhere styles_font_dropdown action is called.
 		 * @example <code>do_action( 'styles_font_dropdown' );</code>
 		 */
-		add_action( 'styles_font_dropdown', array( $this, 'get_dropdown' ) );
+		add_action( 'styles_font_dropdown', array( $this, 'get_view_dropdown' ) );
 
-		// Readme page
-		add_filter( 'plugin_row_meta', array( $this, 'plugin_row_meta' ), 10, 2 );
-		add_action( 'admin_menu', array( $this, 'add_readme_page' ) );
-
-	}
-
-	/**
-	 * Add additional links to the plugin row
-	 * If we're not running as a plugin, this won't do anything,
-	 * because plugin_basename won't match any active plugin path.
-	 */
-	public function plugin_row_meta( $meta, $basename ) {
-		if ( $basename == $this->plugin_basename ) {
-			$meta[] = '<a href="' . network_admin_url( 'plugins.php?page=' . $this->readme_page_slug ) . '">How to use this plugin</a>';
-		}
-		return $meta;
 	}
 
 	public function print_scripts() {
@@ -100,28 +90,9 @@ class Styles_Font_Dropdown {
 	}
 
 	/**
-	 * Display readme and working example in WordPress admin
-	 * Does not add a menu item
-	 * @link /wp-admin/plugins.php?page=styles-font-dropdown
-	 */
-	public function add_readme_page() {
-		add_submenu_page( null, 'Font Dropdown Menu', 'Font Dropdown Menu', 'manage_options', $this->readme_page_slug, array( $this, 'readme_page' ) );
-	}
-
-	/**
-	 * Display views/readme.php, which modifies readme.md to show a working example.
-	 */
-	public function readme_page() {
-		if ( !function_exists( 'Markdown' ) ) {
-			require_once dirname( __FILE__ ) . '/markdown/markdown.php';
-		}
-		$this->get_view( 'readme' );
-	}
-
-	/**
 	 * Display views/dropdown.php
 	 */
-	public function get_dropdown() {
+	public function get_view_dropdown() {
 		$this->get_view( 'dropdown' );
 	}
 
